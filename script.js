4c9e77fee26ca42a9855c95c1fa64e315b95b78b
Carlos <carlosleon0831@gmail.com>
2022-08-15 12:13:53 -0500

    Implement rating system

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const mainContainer = document.querySelector(".card__container");
 const thanksContainer = document.querySelector(".thank-you");
 const submitButton = document.getElementById("submit");
+const rates = document.querySelectorAll(".rate__button");
+const rating = document.getElementById("rating");
 
 submitButton.addEventListener("click", () => {
   thanksContainer.classList.remove("hidden");
-  thanksContainer.classList.add("not-hidden")
+  thanksContainer.classList.add("not-hidden");
   mainContainer.classList.remove("not-hidden");
   mainContainer.classList.add("hidden");
+});
+
+rates.forEach((rate) => {
+  rate.addEventListener("click", () => {
+    rating.innerHTML = rate.innerHTML;
+  });
 });
